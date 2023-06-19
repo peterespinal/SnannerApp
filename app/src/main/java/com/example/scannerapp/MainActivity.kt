a9769bfa96f9cb3cb82b2ab220e49d397407e019
peter segura <peterespinal11@gmail.com>
2023-06-19 11:02:15 -0400

    commit for setup Binding

--- app/src/main/java/com/example/scannerapp/MainActivity.kt
+++ app/src/main/java/com/example/scannerapp/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.scannerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.scannerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
