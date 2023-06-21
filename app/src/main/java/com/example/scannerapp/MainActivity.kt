49e22a908611b80f650230d870a28b03b260c564
peter segura <peterespinal11@gmail.com>
2023-06-20 20:22:10 -0400

    in this commit the app was setup bottom navigation view and replace the toolbar for custom action bar, Navigation its work good in this version

--- app/src/main/java/com/example/scannerapp/MainActivity.kt
+++ app/src/main/java/com/example/scannerapp/MainActivity.kt
@@ -2,14 +2,27 @@ package com.example.scannerapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.scannerapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
+    private  lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val bottomNav = binding.bottomView
+        val toolbar =binding.mainToolbar
+        setSupportActionBar(toolbar)
+        setupActionBarWithNavController(navController)
+        bottomNav.setupWithNavController(navController)
     }
 }
